427eb305fdc63aa0e0bd0dd824313c88ae4733ee
sanghyuk0214 <02lsh14@naver.com>
2023-05-09 14:40:41 +0900

    Initial Commit

---
+++ CMHW7/CMHW7App.swift
@@ -0,0 +1,17 @@
+//
+//  CMHW7App.swift
+//  CMHW7
+//
+//  Created by snlcom on 2023/05/09.
+//
+
+import SwiftUI
+
+@main
+struct CMHW7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
